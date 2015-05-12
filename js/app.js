1f35c48ad4d6845324b36bd863cb66b7be51f05a
Manuel <manuel.casasbarrado@gmail.com>
2015-05-12 16:14:42 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,99 @@
+(function(window) {
+  'use strict';
+
+  function debug(str) {
+    console.log('MANU ContactsService -*-:' + str);
+  }
+
+  // Ok, this kinda sucks because most APIs (and settings is one of them) cannot
+  // be accessed from outside the main thread. So basically everything has to go
+  // down to the SW thread, then back up here for processing, then back down to
+  // be sent to the client. Yay us!
+  var _observers = {};
+
+  var processSWRequest = function(channel, evt) {
+
+    var _contacts = navigator.mozContacts;
+    // We can get:
+    // * oncontactchange
+    // * getAll
+    // * methodName
+    var remotePortId = evt.data.remotePortId;
+    var request = evt.data.remoteData;
+    var requestOp = request.data;
+
+    function listenerTemplate(evt) {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: {
+          id: request.id,
+          event: {
+            contactID: evt.contactID,
+            reason: evt.reason
+          }
+        }
+      });
+    }
+
+    if (requestOp.operation === 'oncontactchange') {
+      _contacts.oncontactchange = listenerTemplate;
+    } else if (requestOp.operation === 'getAll') {
+      var cursor = _contacts.getAll.apply(_contacts, requestOp.params);
+      var contacts = [];
+
+      cursor.onsuccess = () => {
+        var contact = cursor.result;
+        // 'cursor.done' flag should be activated when the last file is
+        // reached. However, it seems that the flag is only is enabled in 
+        // the next iteration so we've always got an undefined file
+        if (typeof contact !== 'undefined') {
+          contacts.push(contact);
+        }
+
+        if (!cursor.done) {
+          cursor.continue();
+        } else {
+          console.info(contacts);
+          // Send message
+          channel.postMessage({
+            remotePortId: remotePortId,
+            data: { id : request.id, result: contacts }}
+          );
+        }
+      };
+
+      cursor.onerror = () => {
+        channel.postMessage({
+          remotePortId: remotePortId,
+          data: { id : request.id, error: cursor.error }}
+        );
+      };
+    } else {
+      _contacts[requestOp.operation].apply(_contacts, requestOp.params).
+        then(result => {
+          channel.postMessage({
+            remotePortId: remotePortId,
+            data: { id : request.id, result: result}}
+          );
+      }).catch(error => {
+        channel.postMessage({
+          remotePortId: remotePortId,
+          data: { id : request.id, error: error.name}}
+        );
+      });
+    }
+  };
+
+
+  // Testing purpose only!!!!
+  window.addEventListener('load', function () {
+    if (window.ServiceHelper) {
+      debug('APP serviceWorker in navigator');
+      window.ServiceHelper.register(processSWRequest);
+    } else {
+      debug('APP navigator does not have ServiceWorker');
+      return;
+    }
+  });
+
+})(window);
